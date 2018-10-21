d61e9584f05f2b6befeec2826563260733107d7f
TNDStudios <joewalters999@gmail.com>
2018-10-21 23:56:03 +0100

    Using the Activator to create a dynamic object of type T before doing the transformation and passing to the calling endpoint

--- TNDStudios.Salesforce.OutboundReceiver/Formatters/PassthroughFormatter.cs
+++ TNDStudios.Salesforce.OutboundReceiver/Formatters/PassthroughFormatter.cs
@@ -59,6 +59,9 @@ namespace TNDStudios.Salesforce.OutboundReceiver.Formatters
         {
             try
             {
+                // Get the outgoing bound type to map to
+                Type modelType = context.ModelType;
+
                 // Load an Xml Document with the incoming stream
                 XmlDocument doc = new XmlDocument();
                 doc.LoadXml(GetBody(context.HttpContext.Request));
@@ -67,11 +70,11 @@ namespace TNDStudios.Salesforce.OutboundReceiver.Formatters
                 string jsonText = JsonConvert.SerializeXmlNode(doc, Formatting.Indented);
 
                 // Create a new outbound message
-                SOAPMessage<SalesforceNotificationsBody> outboundMessage = new SOAPMessage<SalesforceNotificationsBody>();
+                Object outboundMessage = Activator.CreateInstance(modelType);
 
                 // Convert the Json representation to the object
                 outboundMessage = (SOAPMessage<SalesforceNotificationsBody>)JsonConvert.DeserializeObject(jsonText, 
-                    typeof(SOAPMessage<SalesforceNotificationsBody>),
+                    modelType,
                     new JsonSerializerSettings()
                     {
 
